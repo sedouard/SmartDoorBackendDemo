39dc6ebaf04ea98f08eb149acb852c6c4381dc4e
sedouard <steven.edouard1@gmail.com>
2014-08-05 20:53:57 -0700

    debug output

--- service/scheduler/doorbellListener.js
+++ service/scheduler/doorbellListener.js
@@ -57,7 +57,7 @@ function doorbellListener() {
     var date = new Date();
     var time = date.getTime();
     var startSeconds = time / 1000;
-
+    console.log('debug: ' + connectionString);
     var sb = azure.createServiceBusService(connectionString);
  	listenForMessages(c_Timeout);
 
